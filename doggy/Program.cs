e631dbce65221f352b2f8289e4bd5b6455de9f19
DimaSannikov <dimasannikov@gmail.com>
2023-02-22 18:04:59 +0500

    add doggy C#

---
+++ doggy/Program.cs
@@ -0,0 +1,28 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int petSpeed = 5;
+double distance = 10000;
+
+double distanceBetweenFriends = distance;
+int friendSpeed = 0;
+int whichFriend = 2;
+int count = 0;
+
+while(distanceBetweenFriends > 3)
+{
+    if(whichFriend == 2)
+    {
+        friendSpeed = secondFriendSpeed;
+        whichFriend = 1;
+    }
+    else
+    {
+        friendSpeed = firstFriendSpeed;
+        whichFriend = 2;
+    }
+    double meetTime = distanceBetweenFriends / (petSpeed + friendSpeed);
+    distanceBetweenFriends = meetTime * (firstFriendSpeed + secondFriendSpeed);
+    count += 1;
+}
+
+Console.WriteLine(count);
